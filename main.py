8fbb4961c78476919d2b381ac19c9b934ea84f7f
Joao Pedro <joaopedrowatson@hotmail.com>
2021-05-13 16:16:36 -0300

    descobri coisas :D

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
-import os 
+import os
+import random
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-client = discord.Client()
+
+client = commands.Bot(command_prefix='?')
+
 
 @client.event
-async def on_ready():    
-    print(f'{client.user.name} has connected to discordo top')
+async def on_ready():
+    print('Ready :D')
+
+
+@client.command()
+async def embed(ctx):
+    embed = discord.Embed(title='xaxael gostoso', url="",
+                          description='34cm de pau', color=discord.Color.blue())
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def pensa(ctx):
+    if ctx.author == client.user:
+        return
+
+    quotes = [
+        'odeio pobres :gamer:(eu sei que n existe aqui :D é só um teste n ta pronto ainda)',
+        'Não tem problema você ser sulista, tipo, até tem, mas você precisar falar bah?'
+    ]
+
+    response = random.choice(quotes)
+
+    embed = discord.Embed(title='Pensamento', url='',
+                          description=f'{response}', color=discord.Color.blue())
+
+    await ctx.send(embed=embed)
+
+
+# @client.command()
+# async def help(ctx):
+#     response = """
+#     - ?pensa "{frase}" -{autor} → Salvar frase
+#         Retorna → "Grande pensamento salvo! id={id}"
+#     - ?pensa {opt: autor} {opt: id} → Exibe quote
+#         Retorna → {frase}
+#     """
+#     await ctx.send(response)
+
 
 @client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(f'Salve {member.name} bem vindo cachorro muito ICE')
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
 
 client.run(TOKEN)
