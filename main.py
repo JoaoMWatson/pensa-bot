be5e039bbfa5601ab701b22fa62aae86fd4f2455
Joao Pedro <joaopedrowatson@hotmail.com>
2021-05-24 10:46:55 -0300

    reload

--- main.py
+++ main.py
@@ -7,55 +7,35 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='?')
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Ready :D')
 
 
-@client.command()
-async def embed(ctx):
-    embed = discord.Embed(title='xaxael gostoso', url="",
-                          description='34cm de pau', color=discord.Color.blue())
-    await ctx.send(embed=embed)
-
-
-@client.command()
-async def pensa(ctx):
-    if ctx.author == client.user:
+@bot.command(name='quote')
+async def pensamento(ctx):
+    if ctx.author == bot.user:
         return
 
-    quotes = [
+    quotes = {
         'odeio pobres :gamer:(eu sei que n existe aqui :D é só um teste n ta pronto ainda)',
-        'Não tem problema você ser sulista, tipo, até tem, mas você precisar falar bah?'
-    ]
+        'Não tem problema você ser sulista, tipo, até tem, mas você precisar falar bah?',
+    }
 
     response = random.choice(quotes)
 
-    embed = discord.Embed(title='Pensamento', url='',
+    embed = discord.Embed(title='Pensamento',
                           description=f'{response}', color=discord.Color.blue())
 
     await ctx.send(embed=embed)
 
-
-# @client.command()
-# async def help(ctx):
-#     response = """
-#     - ?pensa "{frase}" -{autor} → Salvar frase
-#         Retorna → "Grande pensamento salvo! id={id}"
-#     - ?pensa {opt: autor} {opt: id} → Exibe quote
-#         Retorna → {frase}
-#     """
-#     await ctx.send(response)
+@bot.command(name='register')
+async def registrar(ctx, autor, frase):
+    if ctx.author == bot.user:
+        return
 
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
