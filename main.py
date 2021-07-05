13a8322664757027a61957ead79405ea3d406fea
Joao Pedro <joaopedrowatson@hotmail.com>
2021-07-05 17:36:38 -0300

    chamada por id pronta, melhora no chamado aleatorio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import random
-import discord
 from discord.ext import commands
 from discord.ext.commands.errors import MissingRequiredArgument
 from dotenv import load_dotenv
@@ -21,60 +19,55 @@ async def on_ready():
     print('Ready :D')
 
 
-@bot.command(name='pensamento', help='Frase iconica aleatoria')
-async def pensamento(ctx):
-    try:
-        if ctx.author == bot.user:
-            return
-
-        message = command_funcs.pensamento()
+@bot.command(name='pensa', help='Frase iconica aleatoria')
+async def pensa(ctx, id=None):
+    if ctx.author == bot.user:
+        return
+    
+    if id is None:
+        message = command_funcs.pensa()
         
-        await ctx.channel.send(f"<@{ctx.author.id}>")
-        await ctx.send(embed=message)
+    elif id is not None:
+        message = command_funcs.pensa(id)
 
-    except Exception as e:
-        print(e)
+    await ctx.channel.send(f"<@{ctx.author.id}>")
+    await ctx.send(embed=message)
 
 
 @bot.command(name='autor', help='Paramentro: nome do autor. Listagem de frases desse autor')
 async def autor(ctx, autor: str = ""):
-    try:
-        if ctx.author == bot.user:
-            return
 
-        message = command_funcs.autor(ctx, autor=autor)
+    if ctx.author == bot.user:
+        return
 
-        await ctx.channel.send(f"<@{ctx.author.id}>")
-        await ctx.send(embed=message)
+    message = command_funcs.autor(ctx, autor=autor)
 
-    except Exception as e:
-        print(e)
+    await ctx.channel.send(f"<@{ctx.author.id}>")
+    await ctx.send(embed=message)
 
 
 @bot.command(name='registrar', help='Registra novo pensamento :D')
 async def registrar(ctx, frase, autor):
-    try:
-        if ctx.author == bot.user:
-            return
 
-        message = command_funcs.registrar(ctx, autor, frase)
+    if ctx.author == bot.user:
+        return
 
-        await ctx.channel.send(f"<@{ctx.author.id}>")
-        await ctx.send(embed=message)
-    except Exception as e:
-        print(e)
+    message = command_funcs.registrar(ctx, autor, frase)
+
+    await ctx.channel.send(f"<@{ctx.author.id}>")
+    await ctx.send(embed=message)
 
 
 @bot.command(name='test')
-async def test(ctx, arg):
-    await ctx.send(f'{arg=}')
+async def test(ctx, *args):
+    await ctx.send(f'args {args[0]} ')
 
 
-@bot.event
-async def on_command_error(event, *args, **kwargs):
-    with open('err.txt', 'a') as f:
-        f.write(f'\nUnhandled message: {args[0]} - timestamp: {TIME_NOW}')
-    print(args)
+# @bot.event
+# async def on_command_error(event, *args, **kwargs):
+#     with open('err.txt', 'a') as f:
+#         f.write(f'\nUnhandled message: {args[0]} - timestamp: {TIME_NOW}')
+#     print(args)
 
 
 def main():
